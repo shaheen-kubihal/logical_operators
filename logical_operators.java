86d81d306fa48fa5a952be162d261fd236385769
shaheen-kubihal <shaheenkubihal@gmail.com>
2025-04-11 09:29:57 +0530

    logical_operators.java

---
+++ logical_operators.java
@@ -0,0 +1,8 @@
+class Logical_operators {
+  public static void main(String[] args) {
+    boolean a = true, b = false;
+    System.out.println(a && b); // false
+    System.out.println(a || b); // true
+    System.out.println(!a); // false
+  }
+}
